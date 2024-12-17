c088ab605d01f281a1cc547e6324cba3df7b7091
Jakob <jpotter4@uccs.edu>
2024-12-17 17:12:05 +0000

    Initial commit

--- Final/finalscript.sh
+++ Final/finalscript.sh
@@ -4,3 +4,11 @@
 #Date: 12/17/2024
 #:color desert
 
+apt -y update
+apt install -y nginx
+service nginx start
+
+sed -i 's/Welcome to nginx/ Welcome to Jakob'\''s page/g' /var/www/html/*.html
+
+service nginx stop
+service nginx start
